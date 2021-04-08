19348a0a7ae0a98dbc90779b9d8d10438af95f7c
JOBG290 <80802491+JOBG290@users.noreply.github.com>
2021-04-09 00:59:46 +0900

    Update t1.php

--- t1.php
+++ t1.php
@@ -1,5 +1,17 @@
+<meta charset="utf-8">
+<html lang="ko" dir="ltr">
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
 
 <?php
 
 echo "삼각형의 면적".':'.$_POST["a"].'X'.$_POST["b"].'/2'."=".$_POST["a"] * $_POST["b"] / 2;
-?>
+?> 
+
+
+</body>
+</html>
